4abcffc20640c9a14d3c785a0cd76817a3e02192
mvhsin <shelly.hung@gmail.com>
2019-01-21 11:43:30 +0800

    Add files via upload

--- TFModel_parser.py
+++ TFModel_parser.py
@@ -69,11 +69,12 @@ def main(_):
   with tf.Graph().as_default():
     sess = tf.Session()
     print('load graph:', FLAGS.pb)
-    #g = load_pb(FLAGS.pb)
     load_model(FLAGS.pb)
+    #g = load_pb(FLAGS.pb)
     g = sess.graph
-    print('length', len(g.get_operations()))
+    print('# of ops:', len(g.get_operations()))
 
+    # dump data for tensorboard
     if FLAGS.tb_path:
         writer = tf.summary.FileWriter(FLAGS.tb_path, graph = g)
 
@@ -95,31 +96,40 @@ def main(_):
     with open(FLAGS.dump_ops_path, 'w') as file:
         file.write(strNodeNames)
 
-    strFlopsInfo = "Layer, Filter Num, Filter H, Filter W, Params\n"
+    #dump flops
+    strFlopsInfo = "Layer, Filter Num, Filter H, Filter W, Filter D, Output H, Output W, " \
+                   "Params (N*H*W*D), FLOPs (Params * output_dim^2 * 2)\n"
     lstConv2D = [n for n in g.as_graph_def().node if n.op=='Conv2D']
+    print('# of Conv2D:', len(lstConv2D))
     for node in lstConv2D:
-        print('[_calc_conv_flops]node.name', node.name)
+        #print('[_calc_conv_flops]node.name', node.name)
         strFlopsInfo += node.name + ","
         input_shape = graph_util.tensor_shape_from_node_def_name(g, node.input[0])
-        print('[_calc_conv_flops]input_shape.as_list()', input_shape.as_list())
-        print('[_calc_conv_flops]node.input[0]', input_shape)
+        #print('[_calc_conv_flops]input_shape.as_list()', input_shape.as_list())
+        #print('[_calc_conv_flops]node.input[0]', input_shape)
         filter_shape = graph_util.tensor_shape_from_node_def_name(g, node.input[1])
-        print('[_calc_conv_flops]node.input[1]', filter_shape)
+        #print('[_calc_conv_flops]node.input[1]', filter_shape)
         output_shape = graph_util.tensor_shape_from_node_def_name(g, node.name)
-        print('[_calc_conv_flops]output_shape', output_shape)
+        #print('[_calc_conv_flops]output_shape', output_shape)
         filter_height = int(filter_shape[0])
         filter_width = int(filter_shape[1])
         filter_in_depth = int(filter_shape[2])
         filter_num = int(filter_shape[3])
         params = filter_in_depth * filter_height * filter_width * filter_num
-        print('[_calc_conv_flops]h:%d w:%d d:%d n:%d'% (filter_height, filter_width, filter_in_depth, filter_num))
-        strFlopsInfo += "," + str(filter_height) + "," + str(filter_width) + "," + str(params) + "\n"
-        print('[_calc_conv_flops]params:%d'% params)
-        print('[_calc_conv_flops]output_shape.as_list()', output_shape.as_list())
+        #print('[_calc_conv_flops]h:%d w:%d d:%d n:%d'% (filter_height, filter_width, filter_in_depth, filter_num))
+        strFlopsInfo += str(filter_num) + "," + str(filter_height) + "," \
+                      + str(filter_width) + "," + str(filter_in_depth) + ","
+        #print('[_calc_conv_flops]params:%d'% params)
+        #print('[_calc_conv_flops]output_shape.as_list()', output_shape.as_list())
         output_count = np.prod(output_shape.as_list()[1:], dtype=np.int64)
         output_dim = output_shape.as_list()[1:2]
-        #output_count = np.prod(output_shape.as_list(), dtype=np.int64)
-        print('[_calc_conv_flops]output_count', output_shape.as_list()[1:])    
+        strFlopsInfo += str(output_dim[0]) + "," + str(output_dim[0]) + ","
+        strFlopsInfo += str(params) + ","
+        #print('[_calc_conv_flops]output_count', output_shape.as_list()[1:]) 
+        flops = output_count * filter_in_depth * filter_height * filter_width * 2
+        #print('[_calc_conv_flops]flops', flops) 
+        strFlopsInfo += str(flops) + "\n"
+          
     with open(FLAGS.dump_flops_path, 'w') as file:
         file.write(strFlopsInfo)
                 
@@ -146,57 +156,14 @@ def main(_):
 #        strWts += "Value - " + str(tensor_util.MakeNdarray(n.attr['value'].tensor)) + "\n"
 #    with open(FLAGS.dump_weights_path, 'w') as file:
 #        file.write(strWts)
-'''
-  from tensorflow.python.framework import ops as ops
-  #g=tf.get_default_graph()
-  i = 1
-  for op in g.get_operations():
-#    print('[%d]op.name:%s [%s]' % (i, op.name, op.type))
-    if op.type in ['Conv2D']:
-#      print('[%d]op.name:%s [%s]' % (i, op.name, op.type))
-      flops = ops.get_stats_for_node_def(g, op.node_def, 'flops').value
-#      print('flops:', flops)
-      if flops is not None:
-        print('[%d]op.name:%s [%s] \t %d' % (i, op.name, op.type, flops))
-#        print('[%s] %d' % (op.type, flops))
-#        print('op.vars:', op)
-      i = i + 1
 
-#  with g2.as_default():
-#    flops = tf.profiler.profile(g2, options = tf.profiler.ProfileOptionBuilder.float_operation())
-#    print('FLOP after freezing', flops.total_float_ops)
-'''
-'''
-# ***** (1) Create Graph *****
-g = tf.Graph()
-sess = tf.Session(graph=g)
-with g.as_default():
-    A = tf.Variable(initial_value=tf.random_normal([25, 16]))
-    B = tf.Variable(initial_value=tf.random_normal([16, 9]))
-    C = tf.matmul(A, B, name='output')
-    sess.run(tf.global_variables_initializer())
-    flops = tf.profiler.profile(g, options = tf.profiler.ProfileOptionBuilder.float_operation())
-    print('FLOP before freezing', flops.total_float_ops)
-# *****************************        
-
-# ***** (2) freeze graph *****
-output_graph_def = graph_util.convert_variables_to_constants(sess, g.as_graph_def(), ['output'])
-
-with tf.gfile.GFile('graph.pb', "wb") as f:
-    f.write(output_graph_def.SerializeToString())
-# *****************************
-# ***** (3) Load frozen graph *****
-g2 = load_pb('./graph.pb')
-with g2.as_default():
-    flops = tf.profiler.profile(g2, options = tf.profiler.ProfileOptionBuilder.float_operation())
-    print('FLOP after freezing', flops.total_float_ops)
-'''
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument(
       '--pb',
       type=str,
 #      default='/home/shellyh/Data/Code/python/squeezenet/quantized_sqnet_0105.pb',
+#      default='/home/shellyh/Data/Code/python/InceptionResnetV120180723.pb',
       default='/home/shellyh/Data/Code/python/squeezenet/saved_model.pb',
       help='path of .pb file')
   parser.add_argument(
